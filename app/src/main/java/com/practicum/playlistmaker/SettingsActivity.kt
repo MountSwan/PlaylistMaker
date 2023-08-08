cea5256b6bea29c52c63620ca32dd264032df5ac
Eugene Lebedev <lebedeve@yandex.ru>
2023-08-09 02:31:18 +0500

    Домашнее задание спринта №9

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlistmaker
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +11,34 @@ class SettingsActivity : AppCompatActivity()  {
         setContentView(R.layout.activity_settings)
 
         val arrowBackImage = findViewById<ImageView>(R.id.arrow_back_image)
+        val shareImage = findViewById<ImageView>(R.id.share)
+        val supportImage = findViewById<ImageView>(R.id.support)
+        val userAgreementImage = findViewById<ImageView>(R.id.user_agreement)
 
         arrowBackImage.setOnClickListener {
             finish()
+        }
+
+        shareImage.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message))
+            startActivity(shareIntent)
+        }
+
+        supportImage.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_address)))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_message))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+            startActivity(supportIntent)
+        }
+
+        userAgreementImage.setOnClickListener {
+            val url = Uri.parse(getString(R.string.url_agreement))
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(userAgreementIntent)
         }
     }
 }
